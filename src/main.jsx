fd144b17a4f9f927b802334dae1e9b96c3107535
Alin Moale <moalealin15@gmail.com>
2024-06-05 11:44:32 +0300

    adding sentry to the app

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,28 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+//...
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://d48e468c8df34c2ebbefee6bd4d7be78@o4507373430636544.ingest.de.sentry.io/4507373441187920",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    // Sentry.metrics.metricsAggregatorIntegration(),
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect: React.useEffect,
+    }),
+    Sentry.replayIntegration(),
+  ],
+ 
+  tracesSampleRate: 1.0, 
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0, 
+});
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
